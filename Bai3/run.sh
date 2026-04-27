2750fc1e6be03b07b144045424acfdb3ba5f8f91
25020308-DangGiaPhong <25020308@vnu.edu.vn>
2026-04-27 13:36:03 +0700

    Add files via upload

--- Bai3/run.sh
+++ Bai3/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# 1. Lấy đường dẫn của thư mục hiện tại (nơi chứa file run.sh)
-ROOT_DIR=$(pwd)
+# 1. Lấy chính xác đường dẫn của thư mục đang chứa file run.sh
+ROOT_DIR="$(dirname "$0")"
 
-# 2. Di chuyển vào đúng thư mục đó (để đảm bảo tìm thấy file pom.xml)
+# 2. Di chuyển vào đúng thư mục đó (để đảm bảo Maven luôn tìm thấy file pom.xml)
 cd "$ROOT_DIR"
 
-# 3. Chạy lệnh Maven để dọn dẹp, tải thư viện, biên dịch code và chạy các bài test
-mvn clean test
+# 3. Chạy lệnh Maven để dọn dẹp, tải thư viện, biên dịch code, chạy test và đóng gói thành .jar
+mvn clean package
